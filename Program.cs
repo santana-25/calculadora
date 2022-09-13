db9d380afef549fea8b0f114a14fd1719274c959
Guilherme Santana De Carvalho <74467597+santana-25@users.noreply.github.com>
2022-09-13 16:31:06 -0300

    calculadora V.0.0001

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    ///isso é um teste do git
+    static void Main()
+    {
+        Console.Write("escolha um numero: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("escolha outro numero: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("esolha uma operação");
+        Console.WriteLine(" digite 1 para soma \r\n digite 2 para subtrair \r\n digite 3 para multiplicar \r\n digite 4 para dividir");
+       int calculo = Convert.ToInt32(Console.ReadLine());
+        
+        if (calculo == 1) 
+        {
+         double soma = numero1 + numero2;
+         Console.WriteLine($"a soma deu {soma}");
+        }
+        else if (calculo ==2)
+        {
+           double soma = numero1 - numero2;
+         Console.WriteLine($"a soma deu {soma}"); 
+        }
+        else if (calculo ==3)
+        {
+           double soma = numero1 * numero2;
+         Console.WriteLine($"a soma deu {soma}"); 
+        }
+        else if (calculo ==4)
+        {
+            if (numero1 ==0 || numero2 ==0){
+                Console.WriteLine("não existe divisão por 0");
+            }
+           double soma = numero1 / numero2;
+         Console.WriteLine($"a soma deu {soma}"); 
+        }
+    }
+}
